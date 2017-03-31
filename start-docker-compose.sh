444c9453d23683d3c2744f8a823a60bb32eb9c20
mflores <marcelo.flores.villablanca@gmail.com>
2017-03-31 11:14:43 -0300

    docker files modification

--- start-docker-compose.sh
+++ start-docker-compose.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # docker-compose doesn't work with argument passed by shell command very well, so create temparary variables and pass them to docker-compose up.
-# variable for MS_TASK
-export MS_TASK_HOME=$(pwd)/module-task
+# variable for MS_STORE
+export MS_STORE_HOME=$(pwd)
 
 
-cd $MS_TASK_HOME
+cd $MS_STORE_HOME
 mvn clean
 mvn package
 
